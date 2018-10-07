2b550e0dfed1455121600f7a593d9c959ebb19ac
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-10-07 16:58:28 -0300

    Refactoring Game class adding FramesManager module

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,12 @@
-require 'frame'
+require_relative 'managers/frames_manager'
 class Game
+
+  include FramesManager
+
   attr_reader :player, :frames, :score
 
-  def initialize(player:, frame_klass: 'Frame')
+  def initialize(player:)
     @player = player
-    @frame_klass = frame_klass
     @frames = []
     @score = 0
     @queue_frame_bonus = []
@@ -12,7 +14,7 @@ class Game
   end
 
   def play(pitch)
-    frame = current_frame
+    frame = current_frame frames
 
     remove_from_queue_frame pitch
 
@@ -27,31 +29,23 @@ class Game
 
   private
 
-  def frame_klass
-    @frame_klass.constantize
-  end
-
-  def frame_id
-    size = frames.size
-    return 1 if size == 0
-    size + 1
-  end
+  attr_reader :queue_frame_bonus, :last_pitches
 
   def update_score
     @score = frames.last.score if frames.last.score
   end
 
   def add_to_queue_frame(frame)
-    @queue_frame_bonus.push frame if frame.strike? || frame.spare?
+    queue_frame_bonus.push frame if frame.strike? || frame.spare?
   end
 
   def add_last_pitch(pitch)
-    @last_pitches.push pitch unless @queue_frame_bonus.empty?
+    last_pitches.push pitch unless queue_frame_bonus.empty?
   end
 
   def remove_from_queue_frame(pitch)
-    return if @queue_frame_bonus.size == 0
-    total_last_pitches = @last_pitches + [pitch]
+    return if queue_frame_bonus.size == 0
+    total_last_pitches = last_pitches + [pitch]
 
     if can_run_strike_rule(total_last_pitches)
       set_bonus_to_frame(total_last_pitches)
@@ -65,33 +59,19 @@ class Game
   end
 
   def can_run_strike_rule(total_last_pitches)
-    @queue_frame_bonus.first.strike? && total_last_pitches.size == 2
+    queue_frame_bonus.first.strike? && total_last_pitches.size == 2
   end
 
   def can_run_spare_rule(total_last_pitches)
-    @queue_frame_bonus.first.spare? && total_last_pitches.size == 1
+    queue_frame_bonus.first.spare? && total_last_pitches.size == 1
   end
 
   def set_bonus_to_frame(total_last_pitches)
     bonus = total_last_pitches.sum(&:pins_knocked_down)
-    frame_with_bonus = frames[frames.index(@queue_frame_bonus.first)]
+    frame_with_bonus = frames[frames.index(queue_frame_bonus.first)]
     frame_with_bonus.score_with_plus bonus
-    @queue_frame_bonus.shift
-    @last_pitches.pop
+    queue_frame_bonus.shift
+    last_pitches.pop
   end
 
-  def current_frame
-    last_frame = frames.last
-    return frame_klass.new(id: frame_id) if last_frame.nil?
-
-    is_last_frame_closed = last_frame.strike? || last_frame.spare? || last_frame.closed?
-
-    if is_last_frame_closed
-      new_frame = frame_klass.new(id: frame_id)
-      new_frame.previous_frame = last_frame
-      return new_frame
-    end
-
-    last_frame
-  end
 end
---
+++ lib/managers/frames_manager.rb
@@ -0,0 +1,35 @@
+require 'frame'
+
+module FramesManager
+
+  def current_frame(frames)
+    last_frame = frames.last
+
+    return get_new_frame if last_frame.nil?
+    return new_frame_with_previous(last_frame) if is_frame_closed?(last_frame)
+
+    last_frame
+  end
+
+  private
+
+  def get_new_frame
+    Frame.new(id: frame_id)
+  end
+
+  def frame_id
+    size = frames.size
+    return 1 if size == 0
+    size + 1
+  end
+
+  def is_frame_closed?(frame)
+    frame.strike? || frame.spare? || frame.closed?
+  end
+
+  def new_frame_with_previous(previous_frame)
+    new_frame = get_new_frame
+    new_frame.previous_frame = previous_frame
+    new_frame
+  end
+end
