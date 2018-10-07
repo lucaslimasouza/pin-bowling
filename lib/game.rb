71c02d236d8e157f4c7c473b81b4ea359e902e83
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-10-07 10:51:38 -0300

    Game#play - add a new Frame with Pitch

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,28 @@
+require 'frame'
 class Game
-  attr_reader :player
+  attr_reader :player, :frames
 
-  def initialize(player:)
+  def initialize(player:, frame_klass: 'Frame')
     @player = player
+    @frames = []
+    @frame_klass = frame_klass
+  end
+
+  def play(pitch)
+    frame = frame_klass.new(id: frame_id)
+    frame.add_pitch pitch
+    frames.push frame
+  end
+
+  private
+
+  def frame_klass
+    @frame_klass.constantize
+  end
+
+  def frame_id
+    size = frames.size
+    return 1 if size == 0
+    size + 1
   end
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'game'
+require 'pitch'
 
 RSpec.describe Game do
   subject { build(:game)}
@@ -7,6 +8,24 @@ RSpec.describe Game do
   describe '#player' do
     it 'has the name of player' do
       expect(subject.player).to eq 'User Name'
+    end
+  end
+
+  describe '#play' do
+    it "adds an new Frame" do
+      pitch = build(:pitch)
+      subject.play pitch
+
+      expect(subject.frames.size).to eq 1
+      expect(subject.frames.first.id).to eq 1
+    end
+
+    it "adds a Pitch in new Frame" do
+      pitch = build(:pitch)
+      subject.play pitch
+      added_pitch = subject.frames.first.pitches.first
+
+      expect(added_pitch).to eq pitch
     end
   end
 end
